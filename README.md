9e71e9e5c18007a37a5aa9641836d1614042d348
mbausche <michael.bauschert@googlemail.com>
2023-06-29 15:34:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tabviewDemo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/usaagv)
---
+++ src/app/demo/tab-view-basic-demo.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tab-view-basic-demo',
+    templateUrl: './tab-view-basic-demo.html'
+})
+export class TabViewBasicDemo {}
